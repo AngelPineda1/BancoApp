f51a1bc3d0f6b58b83406b1d9feef35ad4450a12
AngelPineda1 <espinoaangel89@gmail.com>
2024-06-10 15:14:10 -0600

    update

--- BancoMexicoWeb/Program.cs
+++ BancoMexicoWeb/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllerRoute(name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseStaticFiles();
+app.UseHttpsRedirection();
 app.Run();
